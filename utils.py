2eb5f1806ae5a87574c3f64c704851c30532d268
nicholas susanto <nicholas.susanto@mail.utoronto.ca>
2022-01-18 18:58:08 -0500

    Added utils and oil.png

---
+++ utils.py
@@ -0,0 +1,7 @@
+from ast import Tuple
+import pygame
+
+def convertPNG(path: str, scale: Tuple(int, int))-> pygame.Surface: 
+    obj = pygame.image.load(path).convert_alpha()
+    obj = pygame.transform.scale(obj, scale)
+    return obj
