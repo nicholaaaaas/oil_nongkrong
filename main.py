37391b2fec5df5e86283ee0a240e0aa230881ca3
Vallens Kho <secure.mail2357@gmail.com>
2022-01-20 17:45:14 -0500

    Fixed class naming, stylistic errors, and other minor changes

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from __future__ import annotations
-from ast import List, Tuple
-from typing import Optional
+from typing import Optional, Tuple
 import pygame
 
 FPS = 60
 METAL_PLATE = 'oil_nongkrong/assets/metal_plate.png'
 
 
-class Game():
+class Simulation:
     """
     Class representing the simulation.
     """
@@ -21,26 +20,25 @@ class Game():
 
     def __init__(self) -> None:
         """
-        Initialize variables for this Class.
+        Initialize variables for this Game instance
         """
         self.width, self.height = 1000, 750
         self.size = (self.width, self.height)
         self.screen = None
         self._running = True
-        
-        
 
-    def new(self):
+    def setup(self):
+        """ Sets up screen and simulation objects. """
         self.screen = pygame.display.set_mode(self.size)
         self.screen.fill((190, 180, 164))
         pygame.display.flip()
-        
+
         # sprites
         self._metal_plate = pygame.image.load(METAL_PLATE).convert_alpha()
         self._metal_plate = pygame.transform.scale(self._metal_plate, (600, 50))
         self.screen.blit(self._metal_plate, (30, 10))
         self.screen.blit(self._metal_plate, (30, 690))
-        
+
     def run(self) -> None:
         """
         Run the Game until it ends or player quits.
@@ -48,14 +46,14 @@ class Game():
         while self._running:
             # pygame.time.wait(1000 // FPS)
             # set background color
-            
+
             self._events()
             self._update()
             self._draw()
 
     def _update(self) -> None:
         """
-        Updates sprite positions
+        Updates object positions and/or properties.
         """
         pass  # TODO
 
@@ -72,16 +70,18 @@ class Game():
 
     def _draw(self) -> None:
         """
-        Draws the screen, grid, and objects/players on the screen
+        Draws the sprite images to the screen.
         """
         # TODO : Implement
 
         pygame.display.flip()
 
+
 if __name__ == "__main__":
-    game = Game()
-    game.new()
-    game.run()
+    simulation = Simulation()
+    simulation.setup()
+    simulation.run()
+
 
 
 
