2798be99ea37ecf5e2c7acc74e4f41af8e67b4d2
nicholas susanto <nicholas.susanto@mail.utoronto.ca>
2022-01-20 20:00:20 -0500

    Added basic horizontal ui slider

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from __future__ import annotations
 from typing import Optional, Tuple
 import pygame
+import pygame_gui
 import utils
 
 FPS = 60
 METAL_PLATE = 'assets/metal_plate.png'
-
 
 class Simulation:
     """
@@ -15,6 +15,10 @@ class Simulation:
     width: int
     height: int
     screen: Optional[pygame.Surface]
+    manager: pygame_gui.UIManager
+    slider: pygame_gui.elements.UIHorizontalSlider
+    clock: pygame.time.Clock
+    _time_delta: float
 
     _running: bool
     _metal_plate: pygame.Surface
@@ -25,14 +29,22 @@ class Simulation:
         """
         self.width, self.height = 1000, 750
         self.size = (self.width, self.height)
-        self.screen = None
         self._running = True
+        self.screen = pygame.display.set_mode(self.size)
+        self.manager = pygame_gui.UIManager(self.size)
+        self.clock = pygame.time.Clock()
+        self.slider = pygame_gui.elements.UIHorizontalSlider(
+            relative_rect=pygame.Rect(0,0,500,50),manager=self.manager,
+            start_value=0, value_range=(0,100))
+ 
 
     def setup(self):
         """ Sets up screen and simulation objects. """
-        self.screen = pygame.display.set_mode(self.size)
         self.screen.fill((190, 180, 164))
         pygame.display.flip()
+
+        # setup the slider to have the value 0
+        self.slider.set_current_value = 0
 
         # sprites
         self._metal_plate = utils.convertPNG(METAL_PLATE, (600, 50))
@@ -46,7 +58,7 @@ class Simulation:
         while self._running:
             # pygame.time.wait(1000 // FPS)
             # set background color
-
+            self._time_delta = self.clock.tick(60)/1000.0
             self._events()
             self._update()
             self._draw()
@@ -61,23 +73,28 @@ class Simulation:
         """
         Event handling of the game window
         """
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self._running = False
             elif event.type == pygame.MOUSEBUTTONUP:
                 pos = pygame.mouse.get_pos()
                 print(pos)
+            self.manager.process_events(event)
+        self.manager.update(self._time_delta)
 
     def _draw(self) -> None:
         """
         Draws the sprite images to the screen.
         """
         # TODO : Implement
+        self.manager.draw_ui(self.screen)
 
         pygame.display.flip()
 
 
 if __name__ == "__main__":
+    pygame.init()
     simulation = Simulation()
     simulation.setup()
     simulation.run()
@@ -85,3 +102,4 @@ if __name__ == "__main__":
 
 
 
+    
